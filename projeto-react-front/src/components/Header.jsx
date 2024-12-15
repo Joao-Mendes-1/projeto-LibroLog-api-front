d925db6d62cd003bd635e67776b8614c48f63e41
Joao Mendes <joao-vitor-o-mendes@hotmail.com>
2024-12-15 15:22:31 -0300

    Tornando o front-end responsivo

--- projeto-react-front/src/components/Header.jsx
+++ projeto-react-front/src/components/Header.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../styles/components/Header.module.css'; // Importando o CSS modularizado
 import libroLogLogo from '../styles/imagens/libro-log-removebg.png';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Estado para controlar o menu
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Alterna o estado do menu
+  };
+
   return (
     <header className={styles.header}>
       <div>
         <img src={libroLogLogo} alt="Logo da LibroLog" />
       </div>
-      <nav className={styles.nav}>
+
+      {/* Ícone do menu sanduíche */}
+      <div className={styles.menuIcon} onClick={toggleMenu}>
+        <div className={`${styles.bar} ${isMenuOpen ? styles.open : ''}`} />
+        <div className={`${styles.bar} ${isMenuOpen ? styles.open : ''}`} />
+        <div className={`${styles.bar} ${isMenuOpen ? styles.open : ''}`} />
+      </div>
+
+      {/* Menu de navegação */}
+      <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}>
         <Link to="/catalogo">Catálogo</Link>
-        <a href="#sobre-nos">Sobre Nós</a> 
-        <a href="#contato">Contato</a> 
+        <a href="#sobre-nos">Sobre Nós</a>
+        <a href="#contato">Contato</a>
       </nav>
     </header>
   );
